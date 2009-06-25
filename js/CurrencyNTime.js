f25f2079e65b6b6295bb0713ec3988c95eabdcff
Colin Clark <colinbdclark@gmail.com>
2009-06-25 20:16:26 +0000

    FLUID-2966: More stubby component implementation.
    svn path=/sandbox/infusion-workshop/trunk/; revision=7428

--- js/CurrencyNTime.js
+++ js/CurrencyNTime.js
@@ -5,6 +5,7 @@ var latte = latte || {};
 (function () {
     
     var setupCurrencyNTime = function (that) {
+        // Create an empty model where we will hold values from the user.
         that.model = {
             startDate: null,
             endDate: null
@@ -14,26 +15,34 @@ var latte = latte || {};
     latte.currencyNTime = function (container, options) {
         var that = fluid.initView("latte.currencyNTime", container, options);
         
-        that.showRates = function (startDate, endDate) {
-            // Fetch
+        that.fetchRates = function (startDate, endDate) {
+            $.ajax({
+               url: "file://../data/rates.json",
+               type: "GET",
+               dataType: "json",
+               data: that.model,
+               success: function (data) {
+                   that.results = data;
+               }
+            });
+            return that.options.sampleData;
         };
         
         setupCurrencyNTime(that);
         return that;
     };
-    
-    fluid.defaults("latte.currencyNTime", {
+   
+    fluid.defaults("latte.currencyNTime", {     
         // All of our default configuration goes here.
-        sampleData: [
-            {
-                "Date":"2009\/01\/01",
-                "Rate":"1.45010"
-            },
-            {
-                "Date": "2009\/01\/02",
-                "Rate": "0.05"
-            }
-        ]
+        
+        selectors: {
+            startDateList: ".flc-currencyntime-start",
+            endDateList: ".flc-currencyntime-end"
+        },
+        
+        events: {
+            
+        }
     });
     
 })();
