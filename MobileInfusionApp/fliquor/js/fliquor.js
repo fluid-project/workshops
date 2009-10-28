423ba1621e45ad41014c126f2b9dc6ea8d988913
Justin Obara <obarajustin@gmail.com>
2009-10-28 14:09:36 +0000

    FLUID-3337: Added comments
    svn path=/scratchpad/FSOSS-2009/; revision=8517

--- MobileInfusionApp/fliquor/js/fliquor.js
+++ MobileInfusionApp/fliquor/js/fliquor.js
@@ -16,6 +16,14 @@ var fliquor = fliquor || {};
 
 (function ($) {
     
+    /**
+     * A wrapper for the jquery ajax function.
+     * Sets the dataType to be json
+     * 
+     * @param {Object} url, the url to make the ajax call to
+     * @param {Object} success, the function run on successful completion of the ajax call
+     * @param {Object} error, the function to run if the ajax call fails.
+     */
 	var ajaxCall = function (url, success, error) {
 	    $.ajax({
             url: url,
@@ -25,10 +33,22 @@ var fliquor = fliquor || {};
 	    });
 	};
     
+    /**
+     * Retrieves the template from the markup, stores it in a variable and removes it from the DOM.
+     * 
+     * @param {Object} that, the component
+     */
     var storeTemplate = function (that) {
         that.template = that.locate("template").remove();
     };
     
+    /**
+     * Assembles the raw data from the ajax call into the model used by the component.
+     * It makes use of the fluid string templating to assemble the urls
+     * 
+     * @param {Object} that, the component
+     * @param {Object} data, the raw data returned from the ajax call
+     */
     var mapData = function (that, data) {
         if (data.stat === "ok") {
             var photos = data.photos.photo;
@@ -42,6 +62,12 @@ var fliquor = fliquor || {};
         }
     };
     
+    /**
+     * Inserts the normalized data into the template and appends it to the images section of the markup
+     * 
+     * @param {Object} that, the component
+     * @param {Object} data, the normalized data
+     */
     var renderData = function (that, data) {
         that.locate("images").empty();
         $.each(data, function (index, object) {
@@ -58,6 +84,11 @@ var fliquor = fliquor || {};
         that.events.afterRender.fire(that, data);
     };
     
+    /**
+     * Adds a click event to the search button and makes the search field keyboard activatable
+     * 
+     * @param {Object} that, the component
+     */
     var activateSearch = function (that) {
         that.locate("searchBox").fluid("activatable", that.search);
         that.locate("searchButton").click(function (evt) {
@@ -66,11 +97,21 @@ var fliquor = fliquor || {};
         });
     };
     
+    /**
+     * Binds the events and listeners that are used in fliquor
+     * 
+     * @param {Object} that, the component
+     */
     var bindEvents = function (that) {
         that.events.afterImagesReturnedFromFlickr.addListener(that.renderData);
         activateSearch(that);
     };
     
+    /**
+     * calls the various functions that need to be run when the app starts.
+     * 
+     * @param {Object} that, the component
+     */
     var setup = function (that) {
         bindEvents(that);
         storeTemplate(that);
@@ -85,17 +126,36 @@ var fliquor = fliquor || {};
     fliquor.imageViewer = function (container, options) {
         var that = fluid.initView("fliquor.imageViewer", container, options);
         
+        /**
+         * Makes an ajax call to the server, passing along the search query to send to flickr
+         * Returns the raw data and passes it along via the firing of the afterImagesReturnedFromFlickr event
+         * 
+         * @param {Object} query, the image search to run on flickr
+         */
         that.getFromFlickr = function (query) {
             var url = String(window.location).replace(".html", ".json") + "?" + query;
             ajaxCall(url, that.events.afterImagesReturnedFromFlickr.fire, that.events.imageFetchError);
         };
         
+        /**
+         * Peforms a search.
+         * It takes the value entered into the search field, and passes it along as the query to flickr
+         * 
+         * Additionally, it will hide the welcome message, display the searching message.
+         */
         that.search = function () {
             that.locate("welcomeMessage").hide();
             that.locate("searchingMessage").show();
             that.getFromFlickr(that.locate("searchBox").val());
         };
         
+        /**
+         * Displays data the data that is returned from flickr
+         * 
+         * Additionally it will hide the searching message after rendering.
+         * 
+         * @param {Object} data, the raw data returned from flickr
+         */
         that.renderData = function (data) {
             var mappedData = mapData(that, data);
                 
